c4fee1352e6d47476a13214e723e0e1a9a7443c9
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-09-09 23:41:44 -0300

    feat: dockerfile e config backend

--- api-estoque/src/routes/Routes.ts
+++ api-estoque/src/routes/Routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 class Routes {
   static define(router: Router): Router {
-    router.use("/ping", (req, res) => {
+    router.use("/ping", (_req, res) => {
       res.status(200).json({ pong: true });
     });
 
