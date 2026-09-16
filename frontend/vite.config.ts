738ff141fc2604227ca122157583602f5d75613e
victor <victormunarim@gmail.com>
2026-09-15 22:47:50 -0300

    fix: expondo porta para rede

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,14 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT_WEB) || 3000,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/setupTests.ts',
-  },
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT_WEB) || 5173,
   },
 });
