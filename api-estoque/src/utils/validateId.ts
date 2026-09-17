6942d45f8125559a6b2304aecdc6a20abe7214a9
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-09-17 08:44:28 -0300

    feat: categoria controller criado

--- api-estoque/src/utils/validateId.ts
+++ api-estoque/src/utils/validateId.ts
@@ -1,3 +1,15 @@
-export default function validateId(id: number): boolean {
-  return !!id && !Number.isNaN(id);
+import { ValidationError } from "../errors/index";
+
+export default function validateId(
+  rawId: number | string | string[] | undefined,
+): number {
+  const value = Array.isArray(rawId) ? rawId[0] : rawId;
+
+  const id = Number(value);
+
+  if (value === undefined || value === "" || Number.isNaN(id)) {
+    throw new ValidationError("ID inválido");
+  }
+
+  return id;
 }
