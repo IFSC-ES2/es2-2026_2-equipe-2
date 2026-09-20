a6a81b273d7b08c06c3ade77657e2f447cbf5656
Isac Lehmkuhl dos Santos <isacldossan@gmail.com>
2026-09-20 20:10:59 -0300

    refactor: removendo templates e criando pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,28 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/theme.css';
 import './App.css';
-import GenericList from './components/templates/GenericList';
-import { getProdutos } from './services/produto.service';
+import Estoque from './pages/Estoque';
 
 function App() {
   return (
-    <div className="App">
-      <header className="app-header p-3 mb-4 border-bottom">
-        <div className="container">
-          <h1 className="h4 mb-0 fw-bold app-header-title">Sistema de Gestão</h1>
-        </div>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+        <header className="app-header p-3 mb-4 border-bottom">
+          <div className="container">
+            <h1 className="h4 mb-0 fw-bold app-header-title">Sistema de Gestão</h1>
+          </div>
+        </header>
 
-      <main>
-        <GenericList title="Listagem dos Produtos" fetchData={getProdutos} />
-      </main>
-    </div>
+        <main>
+          <Routes>
+            <Route path="/estoque" element={<Estoque />} />
+            <Route path="/" element={<Navigate to="/estoque" replace />} />
+            <Route path="*" element={<Navigate to="/estoque" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
